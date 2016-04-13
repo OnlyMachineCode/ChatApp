d06c5a26996a11b0a5158f4b28723abbb0a8a88e
OnlyMachineCode <legojedi21@gmail.com>
2016-04-12 20:13:50 -0700

    Update and rename JCHATserver.js to server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express')
+  , app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server);
+ var colors = require('colors');
+ var fs = require('fs');
+ 
+
+//app.listen(8070);
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+
+
+
+var mysocket = 0;
+//udp server on 41181
+var dgram = require("dgram");
+var host = dgram.createSocket("udp4");
+
+host.on("listening", function () {
+  var address = host.address();
+  console.log("udp server listening " + address.address + ":" + address.port);
+});
+
+
+	
+
+
+
+	
